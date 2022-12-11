4345afed83960857c77f6cdde81e9ae60870ea25
viper23 <htonnycarlos@gmail.com>
2022-12-11 21:13:51 +0200

    add header

---
+++ partials/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description" content="">
+            <meta name="author" content="">
+            <meta name="generator" content="">
+            <title>Online Library</title>
+            <link rel="canonical" href="">
+            <link rel="stylesheet" href="style.css">
+
+
+            <!-- Bootstrap core CSS -->
+            <link href="../asset/css/simple-datatables-style.css" rel="stylesheet" />
+            <link href="../asset/css/styles.css" rel="stylesheet" />
+            <script src="../asset/js/font-awesome-5-all.min.js" crossorigin="anonymous"></script>
+            
+            <!-- Favicons -->
+            <link rel="apple-touch-icon" href="" sizes="180x180">
+            <link rel="icon" href="" sizes="32x32" type="image/png">
+            <link rel="icon" href="" sizes="16x16" type="image/png">
+            <link rel="manifest" href="">
+            <link rel="mask-icon" href="" color="#7952b3">
+            <link rel="icon" href="">
+            <meta name="theme-color" content="#7952b3">
+
+        </head>
+        
+    </html>
