accf7a32b1ebcf68d976ad237701e4980ad9c467
viper23 <htonnycarlos@gmail.com>
2022-12-11 23:25:00 +0200

    database structure sql cmd

---
+++ database/my_libary.sql
@@ -0,0 +1,11 @@
+create database my_libary;
+
+use my_libary;
+
+create table admin (
+    id int(11) not null auto_increment primary key,
+    email varchar(100) not null,
+    password varchar (100) not null
+)
+
+
